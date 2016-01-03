e10778cc880ac859e46df0756bdb90d73749a461
Emre Ozdemir <emrozdmr55@gmail.com>
2016-01-03 13:14:08 +0200

    Base version.

---
+++ HDD Awake C#/WindowsFormsApplication7/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿using System.Windows.Forms;
+namespace HDDAwakeCSharp
+{
+    partial class HDDAwakeCS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            DeleteLayers();
+
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+
+
+        }
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HDDAwakeCS));
+            this.writeButton = new System.Windows.Forms.Button();
+            this.dirButton = new System.Windows.Forms.Button();
+            this.dirTextBox = new System.Windows.Forms.RichTextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // writeButton
+            // 
+            this.writeButton.Location = new System.Drawing.Point(185, 76);
+            this.writeButton.Name = "writeButton";
+            this.writeButton.Size = new System.Drawing.Size(134, 33);
+            this.writeButton.TabIndex = 0;
+            this.writeButton.Text = "Start Writing!";
+            this.writeButton.UseVisualStyleBackColor = true;
+            this.writeButton.Click += new System.EventHandler(this.writeButton_Click);
+            // 
+            // dirButton
+            // 
+            this.dirButton.Location = new System.Drawing.Point(12, 12);
+            this.dirButton.Name = "dirButton";
+            this.dirButton.Size = new System.Drawing.Size(122, 23);
+            this.dirButton.TabIndex = 1;
+            this.dirButton.Text = "Select Directory";
+            this.dirButton.UseVisualStyleBackColor = true;
+            this.dirButton.Click += new System.EventHandler(this.dirButton_Click);
+            // 
+            // dirTextBox
+            // 
+            this.dirTextBox.DetectUrls = false;
+            this.dirTextBox.Location = new System.Drawing.Point(140, 12);
+            this.dirTextBox.MaxLength = 10000000;
+            this.dirTextBox.Multiline = false;
+            this.dirTextBox.Name = "dirTextBox";
+            this.dirTextBox.Size = new System.Drawing.Size(179, 23);
+            this.dirTextBox.TabIndex = 2;
+            this.dirTextBox.Text = "D:\\HDD Awake C#";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 1;
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            this.numericUpDown1.Location = new System.Drawing.Point(204, 41);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.ReadOnly = true;
+            this.numericUpDown1.Size = new System.Drawing.Size(46, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Time Between Each Write In Minutes :";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "HDD Awake C#";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Ebrima", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(11, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "-";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(10, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "-";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // HDDAwakeCS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(333, 121);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.dirTextBox);
+            this.Controls.Add(this.dirButton);
+            this.Controls.Add(this.writeButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "HDDAwakeCS";
+            this.Text = "HDD Awake C#";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.HDDAwakeCS_FormClosing);
+            this.Resize += new System.EventHandler(this.HDDAwakeCS_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button writeButton;
+        private System.Windows.Forms.Button dirButton;
+        private System.Windows.Forms.RichTextBox dirTextBox;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private NotifyIcon notifyIcon1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private Label label2;
+        private Label label3;
+        private Timer timer1;
+    }
+}
+
---
+++ HDD Awake C#/WindowsFormsApplication7/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.AccessControl;
+
+namespace HDDAwakeCSharp
+{
+    public partial class HDDAwakeCS : Form
+    {
+        public HDDAwakeCS()
+        {
+            InitializeComponent();
+
+            string appname = System.AppDomain.CurrentDomain.FriendlyName;
+
+            #region Checking if the process opened before
+            if (IsProcessOpen(appname.Remove(appname.Length - 5))) //Checking if the application is already opened
+            {
+                Environment.Exit(0);
+            }
+            #endregion
+        }
+
+        private string SELECTEDPATH;
+        private string wrtPath;
+        private string strTextFile;
+        protected int writeState = 0;
+        FileStream fs;
+        int timeRemaining = 4;
+        private void CreateLayers()
+        {
+            SELECTEDPATH = dirTextBox.Text.ToString(); // Get the path
+            wrtPath = SELECTEDPATH + "\\Layer1\\Layer2\\Layer3\\Layer4\\Layer5";
+
+            Directory.CreateDirectory(@wrtPath);   // Create folders
+
+            strTextFile = "\\HDD Awake " + DateTime.Now.Date.Day.ToString() + "-" + DateTime.Now.Date.Month.ToString() + "-" + DateTime.Now.Date.Year.ToString() + ".txt";
+            //Set text file name
+
+            fs = File.Create(wrtPath + strTextFile);   //Create text file. I need to use FileStream to close the stream after creating the file. Else, it'll throw exception.
+
+            fs.Dispose();
+        }
+
+        private void DeleteLayers()
+        {
+            if (writeState > 0) // If layers are created.
+            {
+                Directory.Delete(SELECTEDPATH + "\\Layer1", true);
+            }
+        }
+
+        private void writeButton_Click(object sender, EventArgs e)
+        {
+            CreateLayers();
+
+            backgroundWorker1.RunWorkerAsync();
+
+            writeState++;  //increment write state with each click
+
+            writeButton.Enabled = false;
+
+            label2.Text = "Close the program to stop.";
+
+            timer1.Start();
+        }
+
+        private void dirButton_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            dirTextBox.Text = folderBrowserDialog1.SelectedPath.ToString();
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string sleepSec = numericUpDown1.Value.ToString();  //Get string from num-updown
+            double sleepMin = Convert.ToDouble(sleepSec);
+            int slepsec = Convert.ToInt32(sleepMin * 60);
+            TimeSpan timeout = new TimeSpan(0, 0, slepsec);
+
+            while (writeState % 2 != 0) // Work on 1. 3. 5. 7.... clicks 
+            {
+                string phrase = "Written in: " + DateTime.Now.Hour.ToString() + ":" +
+                                                 DateTime.Now.Minute.ToString() + ":" +
+                                                 DateTime.Now.Second.ToString() + " || " +
+                                                 DateTime.Now.Day.ToString() + "-" +
+                                                 DateTime.Now.Month.ToString() + "-" +
+                                                 DateTime.Now.Year.ToString() + "\n";
+
+                File.WriteAllText(wrtPath + strTextFile, phrase); // wrtPath is selected path. strTextFile is file name with date on it.
+
+                System.Threading.Thread.Sleep(timeout); // Wait for the specied time.
+
+
+            }
+        }
+
+        private void HDDAwakeCS_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+                Hide();
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        void HDDAwakeCS_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //Prevent interrupting shut down
+            if (e.CloseReason == CloseReason.WindowsShutDown)
+            {
+                //Stopping infinite loop
+                backgroundWorker1.CancelAsync();
+            }
+        }
+
+        public bool IsProcessOpen(string appProcessName)
+        {
+            int appState = 0;
+
+            foreach (System.Diagnostics.Process clsProcess in System.Diagnostics.Process.GetProcesses())
+            {
+                if (clsProcess.ProcessName.Contains(appProcessName))
+                    appState = appState + 1;
+            }
+
+            if (appState > 1)
+            {
+                MessageBox.Show("The application is already running!");
+                return true;
+            }
+
+            return false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining = timeRemaining - 1;
+                label3.Text = "Minmizing in: " + timeRemaining;
+            }
+            else
+            { 
+                timer1.Stop();
+                label3.Text = "Minimizing in: -";
+                WindowState = FormWindowState.Minimized;
+            }
+        }
+
+    }
+}
